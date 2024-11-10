b6c2784bff119b5fb07d4703f1e117d6bd8f7f73
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-11-10 10:44:54 +0300

    working with surfaces

--- code/main.py
+++ code/main.py
@@ -1,17 +1,33 @@
 import pygame
+from os.path import join
+from random import randint
 
 pygame.init()
-SCREEN_WIDTH = 1289
-SCREEN_HEIGHT = 720
-display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+WINDOW_WIDTH, WINDOW_HEIGHT = 1289, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
+
+surf = pygame.Surface((100, 200))
+surf.fill("orange") # not in the loop
+x = 100
+
+player_surface = pygame.image.load(join("images", "player.png")).convert_alpha()
+
+star_surface = pygame.image.load(join("images", "star.png")).convert_alpha()
+
+star_locs = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    display_surface.fill("blue")
-    pygame.display.update()
+    display_surface.fill("darkgrey") # reset screen
+    for loc in star_locs:
+        display_surface.blit(star_surface, loc)
+    display_surface.blit(player_surface, (x, 150))
+    x += 0.1
+    pygame.display.update() # show changes
+
 pygame.quit()
