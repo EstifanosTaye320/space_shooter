64dff42e67453452f659c0ca0d266127c8276e40
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-11-11 08:25:46 +0300

    movement

--- code/main.py
+++ code/main.py
@@ -7,6 +7,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1289, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
@@ -15,7 +16,8 @@ surf.fill("orange") # not in the loop
 # imports
 player_surface = pygame.image.load(join("images", "player.png")).convert_alpha()
 player_rect = player_surface.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_direction = 1
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 300
 
 star_surface = pygame.image.load(join("images", "star.png")).convert_alpha()
 star_locs = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
@@ -28,6 +30,7 @@ laser_rect = laser_surface.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
 
 # main loop
 while running:
+    dt = clock.tick() / 1000
     # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,18 +39,17 @@ while running:
     # draw game
     display_surface.fill("darkgrey") # reset screen
     for loc in star_locs:
-        display_surface.blit(star_surface, loc)
-
-        
+        display_surface.blit(star_surface, loc)        
     
     display_surface.blit(meteor_surface, meteor_rect)
     display_surface.blit(laser_surface, laser_rect)
 
     # player movement
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction *= -1
-
-    player_rect.left += player_direction*0.4
+    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
+        player_direction.y *= -1
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction.x *= -1
+    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surface, player_rect)
 
     # update screen
